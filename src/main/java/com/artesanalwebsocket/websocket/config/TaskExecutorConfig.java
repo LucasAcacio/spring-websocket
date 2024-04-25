005c279c2c21ba9e5bf8ead346310fa4d8305e0c
lucasacacio <lucas@artesanalinvestimentos.com.br>
2024-04-25 19:47:34 -0300

    Trocando TaskExecutor

--- src/main/java/com/artesanalwebsocket/websocket/config/TaskExecutorConfig.java
+++ src/main/java/com/artesanalwebsocket/websocket/config/TaskExecutorConfig.java
@@ -3,7 +3,8 @@ package com.artesanalwebsocket.websocket.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.task.TaskExecutor;
+import org.springframework.core.task.*;
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
@@ -12,14 +13,14 @@ public class TaskExecutorConfig {
     @Primary
     @Bean(name="taskExecutor")
     public TaskExecutor taskExecutor() {
-        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(2);
-        taskExecutor.setMaxPoolSize(200);
-        taskExecutor.setPrestartAllCoreThreads(true);
-        taskExecutor.setKeepAliveSeconds(60);
-        taskExecutor.setQueueCapacity(1000000);
-        taskExecutor.setAllowCoreThreadTimeOut(true);
-        return taskExecutor;
+//        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+//        taskExecutor.setCorePoolSize(1);
+//        taskExecutor.setMaxPoolSize(1);
+//        taskExecutor.setPrestartAllCoreThreads(true);
+//        taskExecutor.setKeepAliveSeconds(60);
+//        taskExecutor.setQueueCapacity(1000000);
+//        return taskExecutor;
+        return new SimpleAsyncTaskExecutor();
     }
 }
 
